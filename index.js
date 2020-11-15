3eacedade28ef388df2b61cc73cdcfef62e2391e
Natalia <natalia_vileyto@outlook.com>
2020-11-15 21:57:27 +0300

    create

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+
+// app.get('/', (req, res) => {
+//    // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+//    res.render('index', {
+//        title: 'Главная страница',
+//        isHome: true
+//    })
+// });
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
